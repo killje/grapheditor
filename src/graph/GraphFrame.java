d02b9551dbc3021955ff2749bbc0341867c45686
Patrick <Patrick@zolder>
2013-05-31 18:20:43 +0200

    re-write of GraphFrame

--- src/graph/GraphFrame.java
+++ src/graph/GraphFrame.java
@@ -1,6 +1,7 @@
 package graph;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import javax.swing.*;
 
@@ -10,8 +11,6 @@ import javax.swing.*;
  * @author Timo Smit (s2337789)
  */
 public class GraphFrame extends JFrame {
-
-    private JMenuItem itemHideBar;
 
     public GraphFrame() {
         this.init();
@@ -25,48 +24,109 @@ public class GraphFrame extends JFrame {
         Container contentPane = getContentPane();
         contentPane.setLayout(new FlowLayout());
 
-        JMenuBar menuBar = new JMenuBar();
-
-        JMenu menuFile = new JMenu("File");
-        menuFile.setMnemonic(KeyEvent.VK_F);
-        JMenuItem itemLoad = new JMenuItem("Load");
-        itemLoad.setMnemonic(KeyEvent.VK_L);
-        menuFile.add(itemLoad);
-        JMenuItem itemSave = new JMenuItem("Save");
-        itemSave.setMnemonic(KeyEvent.VK_S);
-        menuFile.add(itemSave);
-        menuBar.add(menuFile);
-
-        JMenu menuEdit = new JMenu("Edit");
-        menuEdit.setMnemonic(KeyEvent.VK_E);
-        JMenuItem itemUndo = new JMenuItem("Undo");
-        itemUndo.setMnemonic(KeyEvent.VK_U);
-        menuEdit.add(itemUndo);
-        JMenuItem itemRedo = new JMenuItem("Redo");
-        itemRedo.setMnemonic(KeyEvent.VK_R);
-        menuEdit.add(itemRedo);
-        JMenuItem itemAddVertex = new JMenuItem("Add vertex");
-        itemAddVertex.setMnemonic(KeyEvent.VK_V);
-        menuEdit.add(itemAddVertex);
-        JMenuItem itemAddEdge = new JMenuItem("Add edge");
-        itemAddEdge.setMnemonic(KeyEvent.VK_E);
-        menuEdit.add(itemAddEdge);
-        menuBar.add(menuEdit);
-
-        JMenu menuWindow = new JMenu("Window");
-        menuWindow.setMnemonic(KeyEvent.VK_W);
-        JMenuItem itemHideBar = new JMenuItem("Hide bar");
-        itemHideBar.setMnemonic(KeyEvent.VK_H);
-        menuWindow.add(itemHideBar);
-        menuBar.add(menuWindow);
-
-        setJMenuBar(menuBar);
-
+        setJMenuBar(addMenuBar());
+        this.add(addToolBar());
+        
         JPanel graphPanel = new GraphPanel();
         this.add(graphPanel);
-        
-        
 
         setVisible(true);
     }
+
+    private JToolBar addToolBar() {
+        JToolBar toolBar = new JToolBar();
+        toolBar.add(saveButton());
+        return toolBar;
+    }
+
+    private JMenuBar addMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        
+        JMenu menuFile = addMenu("File",KeyEvent.VK_F);
+        menuFile.add(addMenuItem("Load",KeyEvent.VK_L,new LoadAction()));
+        menuFile.add(addMenuItem("Save",KeyEvent.VK_S,new SaveAction()));
+        menuBar.add(menuFile);
+        
+        JMenu menuEdit = addMenu("Edit",KeyEvent.VK_E);
+        menuEdit.add(addMenuItem("Undo",KeyEvent.VK_U,new UndoAction()));
+        menuEdit.add(addMenuItem("Redo",KeyEvent.VK_R,new RedoAction()));
+        menuEdit.add(addMenuItem("add Vertex",KeyEvent.VK_V,new addVertexAction()));
+        menuEdit.add(addMenuItem("add Edge",KeyEvent.VK_E,new addEdgeAction()));
+        menuBar.add(menuEdit);
+        
+        JMenu menuWindow = addMenu("Window",KeyEvent.VK_W);
+        menuWindow.add(addMenuItem("Hide bar",KeyEvent.VK_H,new HideAction()));
+        menuBar.add(menuWindow);
+        return menuBar;
+    }
+    
+    private JMenu addMenu(String name,int shortKey){
+        JMenu menu = new JMenu(name);
+        menu.setMnemonic(shortKey);
+        return menu;
+    }
+    
+    private Button saveButton() {
+        Button saveButton = new Button("Save");
+        saveButton.addActionListener(new SaveAction());
+        return saveButton;
+    }
+    
+    private JMenuItem addMenuItem(String name,int shortKey,AbstractAction listner){
+        JMenuItem menuItem = new JMenuItem(name);
+        menuItem.addActionListener(listner);
+        menuItem.setMnemonic(shortKey);
+        return menuItem;
+    }
+    
+    private class SaveAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    private class LoadAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    private class UndoAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    private class RedoAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    private class addVertexAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    private class addEdgeAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    private class HideAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+    
 }
